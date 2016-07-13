1e19260868660c555241ab83d160eb10932e798e
Breno Oliveira <breno26@gmail.com>
2016-07-13 00:07:23 -0300

    call Trackandgo.redis

--- spec/trackandgo_spec.rb
+++ spec/trackandgo_spec.rb
@@ -5,6 +5,15 @@ describe Trackandgo do
   context ".redis" do
     context "missing block" do
       it { expect{Trackandgo.redis}.to raise_error ArgumentError }
+      it do
+        count = []
+        expect {
+          Trackandgo.redis do
+            raise Redis::CommandError
+          end
+        }.to raise_error Redis::CommandError
+
+      end
     end
   end
 end
